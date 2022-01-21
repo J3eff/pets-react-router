14edbb4b7d5e2c8f37fded0ddc1007321b6dd4d7
J3eff <jeffersonvideo125@gmail.com>
2022-01-20 23:28:39 -0300

    Criando NavBar

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
 
 function App() {
 
   return (
     <>
       <Router>
+        <Cabecalho />
+
         <Switch>
 
           <Route exact path='/'>
@@ -20,9 +23,9 @@ function App() {
           <Route path='/sobre'>
             <Sobre />
           </Route>
-          
+
           <Route>
-            <Pagina404 /> 
+            <Pagina404 />
           </Route>
 
         </Switch>
